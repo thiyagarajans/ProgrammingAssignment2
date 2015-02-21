c08a67c79558974022459d2e1380b5dd326a724b
Thiyagarajan S <sthiyaga@ibab.ac.in>
2015-02-21 21:00:38 +0530

    Works for a test matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## This script has two functions 1. to create a special matrix and 
 ## 2. to cache the inverse of a matrix
 
-## I admit I have no idea how this is working as of now.
+## Though not clear how this works, I managed to set it to work, just by copying
+# the example code and slightly modifying it.
+
+#Run this code with x <- makeCacheMatrix(matrix(rnorm(9),3,3))
+#This creates the matrix, calcualates the inverse and stores.
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
@@ -20,10 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ##  I just copied the code from the example, I have no idea even how the example
 # works
+#works with a test matrix
 
 cacheSolve <- function(x, ...) {
 	m <-x$getinverse()
-	if(!is.null(m) {
+	if(!is.null(m)) {
 		message("getting cached inverse")
 		return(m)
 	}
